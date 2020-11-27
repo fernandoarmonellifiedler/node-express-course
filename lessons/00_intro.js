242314fdaabdd2ea8adc9beba41a13199d2ca2d8
fernandoarmonellifiedler <fernandoarmonelli@gmail.com>
2020-11-26 23:47:39 -0300

    initial file setup

---
+++ lessons/00_intro.js
@@ -0,0 +1,14 @@
+/* 
+Traditionally, JavaScript is a browser-side programming language. However, Node.js gives you the ability to run JavaScript files on the server-side. Express is a library for Node.js, that allows you to make requests to different "endpoints" and get a response back.
+
+In this tutorial you will learn how to:
+
+Set up a project using Node.js and NPM
+Send JSON data using Express
+Test your API using PostMan
+Secure your routes with middleware
+When you see the bulb symbol, the following info is a optional tip, or context.
+
+This tutorial is interactive. Look for instructions on how to complete each step at the bottom of the issue. If the next step doesn't appear, try refreshing the page.
+
+When you are ready to start, close this issue.*/
---
+++ lessons/01_set-up-node-environment.js
@@ -0,0 +1,54 @@
+/*
+Installation
+First, let's install Node.js, and an app called Postman to test our API. You will be coding along with me, so make sure you also have git installed.
+
+Node.js comes with something called NPM (Node Package Manager) that we will use manage our node modules. You can check if you have Node, NPM, and Git installed by running these commands (one at a time):
+
+node -v
+npm -v
+git --version
+
+You should get a version back from each of these commands. If you get an error, you'll want to check that it was correctly installed.
+
+--------------------------------
+
+Clone Template Repository
+By signing up for this course, a template repository was created on your GitHub account. Clone this repository locally on your computer and navigate to it by running:
+
+git clone https://github.com/fernandoarmonellifiedler/node-express-course.git
+cd node-express-server-intro
+
+--------------------------------
+
+File Setup
+Open the folder you just cloned in your favorite text editor.
+You should have a few files already created:
+
+.gitignore
+server.js
+The server file will be the main one we use. In the .gitignore folder, you should see this line at the top /node_modules
+
+Express (the server library we will install) is a node_module. Rather than track the entire library in our git history, we will create a package.json file, which will allow us to install and save node modules.
+
+Run this from your command line: npm init -y
+
+You should have a package.json file in your folder now.
+
+Now you can install Express using NPM. Enter this command in the terminal: npm install express --save
+
+A folder called node_modules should apear in your project. This is where the actual files for express are stored. If you open the folder, you can see how many files there are. These will only be stored on your machine, because we told git to ignore this entire directory.
+
+In your package.json file and make sure you have something like this under your dependencies:
+
+"dependencies": {
+    "express": "^4.17.1",
+  }
+  
+The number on the right is the version you downloaded. Since we aren't tracking the actual node_modules folder, this section is a reference used to re-install the modules your application depends on.
+
+When you are finished with this section, push your file to GitHub for the next step:
+
+git add .
+git commit -m "initial file setup"
+git push origin master
+*/
