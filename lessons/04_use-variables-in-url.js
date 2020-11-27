f4dc8185ee48673053cf86ed587425c8b2cecf73
fernandoarmonellifiedler <fernandoarmonelli@gmail.com>
2020-11-27 00:41:26 -0300

    add login POST route

--- lessons/04_use-variables-in-url.js
+++ lessons/04_use-variables-in-url.js
@@ -1,3 +1,27 @@
 /*
 Use Variables in URL
+In Express, words with a colon in front of them in the url are treated as variables. You can access the value of each variable through req.params, like this:
+
+app.get('/users/:id',function(req,res){
+	console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id
+	})
+})
+
+Let's test it out!
+
+Add the function above your app.listen function in your server.js file, restart the server, and paste this into your browser url: http://localhost:8000/users/mark
+
+Now check the terminal for the console.log of req.params.id. You should see the name 'mark', since it is the value we passed in place of the 'id' variable. Since we are running this in the terminal, our console.log will appear there (instead of the browser).
+
+After testing that it works, push your changes your GitHub repo:
+
+git add server.js
+git commit -m"add dynamic GET route"
+git push origin master
+
+You can use dynamic variables to search for specific data associated with an id in your database, and return that (instead of just returning the id).
 */
---
+++ lessons/05_making-post-route.js
@@ -0,0 +1,93 @@
+/*
+Making a Post Route
+So far, we have only made GET requests to our server. A POST request can send data securely through the request body. In order to make POST requests, first we need to include the "body-parser" library from our node_modules (included with express). Add these lines after the app variable:
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+Let's write a function to handle a POST request made to the 'login' endpoint, as if a user was trying to log in:
+
+app.post('/login',function(req,res){
+ 	const username=req.body.username;
+ 	const password=req.body.password;
+ 
+ 	const mockUsername="billyTheKid";
+ 	const mockPassword="superSecret";
+ 
+ 	if (username===mockUsername && password===mockPassword){
+      	res.json({
+      	 	success: true,
+      	 	message: 'password and username match!',
+      	 	token: 'encrypted token goes here'
+      	})
+ 	} else {
+      	res.json({
+      	 	success: false,
+      	 	message: 'password and username do not match'
+      	})
+ 	}
+})
+
+Your entire file should look like this now:
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
+
+const mockUserData=[
+	{name:'Mark'},
+	{name:'Jill'}
+]
+
+app.get('/users',function(req,res){
+	res.json({
+		success: true,
+		message: 'successfully got users. Nice!',
+		users: mockUserData
+	})
+})
+// colons are used as variables that be viewed in the params
+app.get('/users/:id',function(req,res){
+	console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id
+	})
+})
+
+app.post('/login',function(req,res){
+	// Typically passwords are encrypted using something like bcrypt before sending to database
+	const username=req.body.username;
+	const password=req.body.password;
+
+	// This should come from the database
+	const mockUsername="billyTheKid";
+	const mockPassword="superSecret";
+
+	if (username===mockUsername && password===mockPassword){
+		// In practice, use JSON web token sign method here to make an encrypted token
+		res.json({
+			success: true,
+			message: 'password and username match!',
+			token: 'encrypted token goes here'
+		})
+	} else {
+		res.json({
+			success: false,
+			message: 'password and username do not match'
+		})
+	}
+
+})
+
+app.listen(8000,function(){console.log('server is listening')})
+
+Time to commit our changes!
+
+git add server.js
+git commit -m"add login POST route"
+git push origin master
+*/
